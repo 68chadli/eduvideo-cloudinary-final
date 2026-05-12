9ceb4b38164e16daeee86944c14a1035815a0808
chadli <68chadli@gmail.com>
2026-05-12 21:52:16 +0200

    Ajout vérification collectstatic dans entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,13 @@
 
 # Attendre que la base soit prête (optionnel)
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
+if [ $? -eq 0 ]; then
+    echo "✅ collectstatic réussi"
+else
+    echo "❌ collectstatic a échoué"
+    exit 1
+fi
 
 # Créer un superutilisateur automatiquement (si non existant)
 echo "from accounts.models import User; User.objects.filter(is_superuser=True).exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
